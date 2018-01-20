e9dc8a40c9b4df21f45b673b6845c7d91e1e5a1d
Kirsten Hunter <synedra@gmail.com>
2018-01-19 16:26:49 -0800

    Stripping release directory

--- release.js
+++ release.js
@@ -28,7 +28,7 @@ exec(`pkg ${source} --target node8-linux-x86,node8-linux-x64,node8-win-x86,node8
         filename =filename.replace('macos','mac')
         filename =filename.replace('x86','386')
         fs.renameSync(oldname,"release/" + filename)
-        require('child_process').execSync(`gpg -s -o release/${filename}.sig release/${filename}`)
+        require('child_process').execSync(`shasum -a 256 release/${filename} | awk '{print $1}' > release/${filename}.sig`)
       }    
     })
     .catch(function (err) {
